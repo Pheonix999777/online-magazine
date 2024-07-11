94b9978f2debd627e1f7f7f6a0aaa6ad0834f92a
Pheonix999777 <Pheonix4777@gmail.com>
2024-07-11 11:19:04 +0500

    change DropDown

--- src/app/Components/Header/Header.jsx
+++ src/app/Components/Header/Header.jsx
@@ -21,7 +21,7 @@ export default function Header() {
 
   const category = [
     { href: "/about", label: "мальчики" },
-    { href: "/delivery-payment", label: "женщины" },
+    { href: "/delivery-payment", label: "девочки" },
     { href: "/pages/categories", label: "младенцы" },
   ];
 
@@ -45,42 +45,40 @@ export default function Header() {
             Get it
           </a>
           <div className="header__content">
-            <button className="header__downBtn">мальчики</button>
+            {category.map((item, index) => (
+              <div key={index} className="header__downMenu">
+                <button className="header__downBtn">{item.label}</button>
+                <div className="header__menu">
+                  <div className="header__megaMenu">
+                    <ul className="header__menu-ul">
+                      <li className="header__menu-list">
+                        <Link
+                          className="header__menu-link"
+                          href={"/pages/categories"}
+                        >
+                          брюки
+                        </Link>
+                      </li>
+                      <li className="header__menu-list">обуви</li>
+                      <li className="header__menu-list">шапки</li>
+                    </ul>
 
-            <button className="header__downBtn">девочки</button>
+                    <ul className="header__menu-ul">
+                      <li className="header__menu-list">финки</li>
+                      <li className="header__menu-list">женщины</li>
+                      <li className="header__menu-list">куртки</li>
+                      <li className="header__menu-list">футболки</li>
+                    </ul>
 
-            <div className="header__downMenu">
-              <button className="header__downBtn">младенцы</button>
-              <div className="header__menu">
-                <div className="header__megaMenu">
-                  <ul className="header__menu-ul">
-                    <li className="header__menu-list">
-                      <Link
-                        className="header__menu-link"
-                        href={"/pages/categories"}
-                      >
-                        брюки
-                      </Link>
-                    </li>
-                    <li className="header__menu-list">обуви</li>
-                    <li className="header__menu-list">шапки</li>
-                  </ul>
-
-                  <ul className="header__menu-ul">
-                    <li className="header__menu-list">финки</li>
-                    <li className="header__menu-list">женщины</li>
-                    <li className="header__menu-list">куртки</li>
-                    <li className="header__menu-list">футболки</li>
-                  </ul>
-
-                  <ul className="header__menu-ul">
-                    <li className="header__menu-list">шорты</li>
-                    <li className="header__menu-list">свитеры</li>
-                    <li className="header__menu-list">аккесуары</li>
-                  </ul>
+                    <ul className="header__menu-ul">
+                      <li className="header__menu-list">шорты</li>
+                      <li className="header__menu-list">свитеры</li>
+                      <li className="header__menu-list">аккесуары</li>
+                    </ul>
+                  </div>
                 </div>
               </div>
-            </div>
+            ))}
 
             <button className="header__category">
               <CgMenuGridO style={{ marginRight: "2px" }} />
